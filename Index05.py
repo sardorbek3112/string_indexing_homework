1e0e51c846853cc968bc8873bfc22df5faf48b88
sardorbek3112 <raupovsardorbek.3107@gmail.com>
2022-07-22 15:55:58 +0500

    Index05.py

--- Index05.py
+++ Index05.py
@@ -1,3 +1,8 @@
+def num(i):
+    k="12345678"
+    if i in k:return 1
+    return 0
+
 def main(s):
     """
     Given a variable s string of length five. Determine the number of digits involved in this variable.
@@ -6,4 +11,4 @@ def main(s):
     Returns:
         int: answer
     """
-    return
+    return num(s[0])+num(s[1])+num(s[2])+num(s[3])+num(s[4])
