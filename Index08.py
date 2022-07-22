51d315f479bcf7186f9bda9e2e5597d2e38266bc
sardorbek3112 <raupovsardorbek.3107@gmail.com>
2022-07-22 16:22:53 +0500

    Index08.py

--- Index08.py
+++ Index08.py
@@ -1,3 +1,7 @@
+def num(i):
+    if i=="*":
+        return 1
+    return 0
 def main(s):
     """
     A string of length five is given. Return the index of the "*" character, return False if not present.
@@ -6,5 +10,6 @@ def main(s):
     Returns:
         int: answer
     """
-    return
-        
+    if "*" in s:
+        return num(a[0]) * 0 + num(a[1]) * 1 + num(a[2]) * 2 + num(a[3]) * 3 + num(a[4]) * 4
+    return False
